41728f4bbaf1ee506f91ec0d2957a830e8540ca9
Afalide <afalide@hotmail.fr>
2015-04-08 14:19:57 +0200

    adds test_area class (same as a 'fixture')

--- include/tuter/macros.hpp
+++ include/tuter/macros.hpp
@@ -2,10 +2,12 @@
 #ifndef TUTER_SYMBOLS_HPP
 #define TUTER_SYMBOLS_HPP
 
+#include "tuter/test_area.hpp"
+
 #include <vector>
 
 #define TUTER_DECLARE_FN_NAME      _tuter_create_test_areas
-#define TUTER_DECLARE_TESTS_BEGIN  std::vector<stuff*> TUTER_DECLARE_FN_NAME () { std::vector<stuff*> __vec;
+#define TUTER_DECLARE_TESTS_BEGIN  std::vector<tuter::test_area*> TUTER_DECLARE_FN_NAME () { std::vector<tuter::test_area*> __vec;
 #define TUTER_DECLARE_TESTS_END    return __vec; }
 #define TUTER_DECLARE(X)           __vec.push_back(new X );
 
---
+++ include/tuter/test_area.hpp
@@ -0,0 +1,18 @@
+
+#ifndef TUTER_TEST_AREA_HPP
+#define TUTER_TEST_AREA_HPP
+
+namespace tuter
+{
+
+class test_area
+{
+
+public:
+    test_area();
+    virtual ~test_area();
+};
+
+}
+
+#endif // TUTER_TEST_AREA_HPP
